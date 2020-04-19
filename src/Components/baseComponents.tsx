13916495e2c262f548fedb3fba943c5b4ba78faa
wyswill <1151373571@1qq.com>
2020-04-19 15:06:12 +0800

    基础组件样式修改

--- src/Components/baseComponents.tsx
+++ src/Components/baseComponents.tsx
@@ -1,38 +1,41 @@
 import React from "react";
 import items from "../config/baseComponents";
-import {
-  Grid, Paper,
-  makeStyles,
-  createStyles,
-  Theme,
-} from '@material-ui/core'
+import { Box, Grid, makeStyles, createStyles, Theme } from "@material-ui/core";
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-  item: {
-    display: 'Grid',
-    gridTemplateColumns: 'repeat(4,25%)'
-  },
-  itme2: {}
-
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    baseComponents: {},
+    item: {
+      display: "Grid",
+      gridTemplateColumns: "repeat(4,25%)",
+      wordBreak: "break-all",
+    },
+    itme2: {
+      minHeight: "40px",
+    },
+  })
+);
+let components = [];
+Object.keys(items).forEach((ele) => {
+  components = components.concat(items[ele]);
+});
 export default function BaseComponents() {
   const classes = useStyles();
-  return (
-      <div className="baseComponents">
-        {
-          Object.keys(items).map((ele, index) => (
-              <Grid container className={classes.item} key={index}>
-                {
-                  items[ele].map(elem => (
-                      <div className={classes.itme2} key={elem}>
-                        {elem}
-                      </div>
-                  ))
-                }
-              </Grid>
-          ))
-        }
-      </div>
-  );
 
+  return (
+    <Grid container className={classes.item}>
+      {components.map((elem) => (
+        <Box boxShadow={3} className={classes.itme2} key={elem}>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            {elem}
+          </Grid>
+        </Box>
+      ))}
+    </Grid>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './home/App';
-import * as serviceWorker from './serviceWorker';
-import './less/mainStyle.less'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./home/App";
+import * as serviceWorker from "./serviceWorker";
+import "./less/mainStyle.less";
 
-ReactDOM.render(
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
